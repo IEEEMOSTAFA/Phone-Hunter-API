3f351c8c17d435d4af982e103033897b8025eaaf
IEEEMOSTAFA <skmostafa8888@gmail.com>
2024-05-11 09:23:21 +0600

    phone details added

--- js/phone.js
+++ js/phone.js
@@ -1,12 +1,12 @@
 // console.log('Phone Hunting')
-const loadPhone = async (searchText, isShowAll) => {
+const loadPhone = async (searchText , isShowAll) => {
   const res = await fetch(` https://openapi.programming-hero.com/api/phones?search=${searchText}`);
   const data = await res.json();
   const phones = data.data;
   displayPhones(phones, isShowAll);
 }
 const displayPhones = (phones, isShowAll) => {
-  console.log(phones);
+  // console.log(phones);
   // 1.
   const phoneContainer = document.getElementById('phone-container');
   // clear phone container cards before adding new cards
@@ -15,13 +15,12 @@ const displayPhones = (phones, isShowAll) => {
   const showAllContainer = document.getElementById('show-all-container');
   if (phones.length > 12 && !isShowAll) {
     showAllContainer.classList.remove('hidden');
-
   }
   else {
     showAllContainer.classList.add('hidden');
   }
 
-  console.log('is show all', isShowAll);
+  // console.log('is show all', isShowAll);
   // display only first 12 phones if not show all
   if (!isShowAll) {
     //  display only first 12 phone
@@ -31,7 +30,7 @@ const displayPhones = (phones, isShowAll) => {
   //  console.log(phones.length);
 
   phones.forEach(phone => {
-    console.log(phone);
+    // console.log(phone);
     // 2.Create a div
     const phoneCard = document.createElement('div');
     phoneCard.classList = `card  bg-gray-100 p-4 shadow-xl`;
@@ -56,12 +55,57 @@ const displayPhones = (phones, isShowAll) => {
 }
 
 
-const handleShowDetail  = async (id) =>{
-  console.log('Click Show Details',id)
+const handleShowDetail = async (id) => {
+  // console.log('Click Show Details',id)
   // load individual or phone data
   const res = await fetch(`https://openapi.programming-hero.com/api/phone/${id}`);
   const data = await res.json();
-  console.log(data);
+  const phone = data.data;
+  // console.log(data);
+  showPhoneDetails(phone);
+}
+
+const showPhoneDetails = (phone) => {
+  console.log(phone);
+  const phoneName = document.getElementById('show-detail-phone-name');
+  phoneName.innerText = phone.name;
+
+  const showDetailContainer = document.getElementById('show-detail-container');
+  showDetailContainer.innerHTML = `
+  <img  src="${phone.image}" alt=""  />
+  <p><span>Storage:</span>${phone?.mainFeatures?.storage}</p>
+  <p><span>ChipSet
+  :</span>${phone?.mainFeatures?.chipSet
+    }</p>
+  <p><span>DisplaySize
+  :</span>${phone?.mainFeatures?.displaySize
+    }</p>
+  <p><span>Memory
+  :</span>${phone?.mainFeatures?.memory
+    }</p>
+ 
+  <p><span>Bluetooth
+  :</span>${phone?.others?.Bluetooth
+    }</p>
+  <p><span>GPS:</span>${phone?.others?.GPS}</p>
+  <p><span>
+  NFC:</span>${phone?.others?.
+      NFC}</p>
+  <p><span>Radio:</span>${phone?.others?.Radio}</p>
+  <p><span>USB
+  :</span>${phone?.others?.USB
+    }</p>
+  <p><span>WLAN
+  :</span>${phone?.others?.WLAN
+    }</p>
+  }</p>
+  <p><span>ReleaseDate:</span>${phone?.releaseDate
+  }</p>
+  <p><span>Slug:</span>${phone?.slug}</p>
+  
+  `
+  // show the Modal
+  show_details_modal.showModal();
 }
 // handle search button
 const handleSearch = (isShowAll) => {
@@ -96,5 +140,4 @@ const handleShowAll = () => {
 
   handleSearch(true);
 }
-// loadPhone();
-
+loadPhone();
